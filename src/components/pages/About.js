43aebcab231489fc5ca5c52ab02932ff83f3f8e6
codingChewie <codingChewie@gmail.com>
2020-11-17 17:08:16 -0600

    21. React Router Setup

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this</h1>
+      <p>App to search</p>
+      <p>version: 111</p>
+    </Fragment>
+  )
+}
+
+export default About
