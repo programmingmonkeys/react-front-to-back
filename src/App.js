87d3bf3fecfd77a85bee63afdd72e02a513ac9aa
codingChewie <codingChewie@gmail.com>
2020-11-18 08:18:17 -0600

    22. Single User Component & Data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
@@ -13,6 +14,7 @@ import './App.css'
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
   }
@@ -22,10 +24,22 @@ class App extends Component {
     this.setState({ loading: true })
 
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+      `https://api.github.com/search/users?q=${text}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
     )
 
     this.setState({ users: res.data.items, loading: false })
+  }
+
+  // Get single Github user
+  getUser = async (username) => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    )
+    console.log(res)
+
+    this.setState({ user: res.data, loading: false })
   }
 
   // Clear users from state
@@ -40,7 +54,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading } = this.state
+    const { users, user, loading } = this.state
 
     return (
       <Router>
@@ -65,6 +79,12 @@ class App extends Component {
                 )}
               />
               <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={(props) => <User {...props} getUser={this.getUser} user={user} />}
+                loading={loading}
+              />
             </Switch>
           </div>
         </div>
