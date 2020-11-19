936145a69b61404d3ef3962d002cfa5763cb7e57
codingChewie <codingChewie@gmail.com>
2020-11-19 10:46:30 -0600

    42. User Model & Validation

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -6,6 +6,9 @@ const app = express()
 // Connect database
 connectDB()
 
+// Init Middleware
+app.use(express.json({ extended: false }))
+
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
