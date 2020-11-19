9234466ff8c3c5df7d109dd1bb6da7570e81bdec
codingChewie <codingChewie@gmail.com>
2020-11-19 09:56:55 -0600

    40. Backend Routes

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -8,6 +8,13 @@ app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to contact keeper' })
 })
 
+const apiVersion = 'v1'
+
+// Define Routes
+app.use(`/api/${apiVersion}/users`, require('./routes/users'))
+app.use(`/api/${apiVersion}/auth`, require('./routes/auth'))
+app.use(`/api/${apiVersion}/contacts`, require('./routes/contacts'))
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
