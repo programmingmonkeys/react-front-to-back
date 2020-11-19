9d285489d63b671b36093f2b727893342cad6975
codingChewie <codingChewie@gmail.com>
2020-11-19 15:20:09 -0600

    48. Add Contact Route

--- contact-keeper/routes/contacts.js
+++ contact-keeper/routes/contacts.js
@@ -13,9 +13,9 @@ const Contact = require('../models/Contact')
 // @access: Private
 router.get('/', auth, async (req, res) => {
   try {
-    const contact = await Contact.find({ users: req.user.id }).sort({ date: -1 })
+    const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 })
 
-    res.json(contact)
+    res.json(contacts)
   } catch (err) {
     console.error(err.message)
     res.status(500).send('Server Error')
@@ -25,8 +25,28 @@ router.get('/', auth, async (req, res) => {
 // @route: POST api/contacts
 // @desc: Add new contact
 // @access: Private
-router.post('/', (req, res) => {
-  res.send('Add contact')
+router.post('/', [auth, [check('name', 'Name is required').not().isEmpty()]], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+  const { name, email, phone, type } = req.body
+
+  try {
+    const newContact = new Contact({
+      name,
+      email,
+      phone,
+      type,
+      user: req.user.id,
+    })
+
+    const contact = await newContact.save()
+
+    res.json(contact)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
 })
 
 // @route: PUT api/contacts/:id
