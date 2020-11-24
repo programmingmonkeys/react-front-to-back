645dac7cc8b0d859e2850599c281e54f6455b69d
codingChewie <codingChewie@gmail.com>
2020-11-24 09:43:31 -0600

    62. Alert Context, State & Component

--- contact-keeper/client/src/App.js
+++ contact-keeper/client/src/App.js
@@ -7,9 +7,11 @@ import Home from './component/pages/Home'
 import About from './component/pages/About'
 import Register from './component/auth/Register'
 import Login from './component/auth/Login'
+import Alerts from './component/layout/Alerts'
 
 import ContactState from './context/contact/ContactState'
 import AuthState from './context/auth/AuthState'
+import AlertState from './context/alert/AlertState'
 
 import './App.css'
 
@@ -17,19 +19,22 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   )
